b89731f2678296a8b76e0a95332597002eefc852
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-24 01:23:27 +0530

    Passing test

--- cucumber-watir/features/step_definitions/amazon_search_step.rb
+++ cucumber-watir/features/step_definitions/amazon_search_step.rb
@@ -9,8 +9,12 @@ When("a user search for {string}") do |string|
 end
 
 Then("amazon should return result for {string}") do |string|
-    @browser.link(:id => "bcKwText").wait_until_present
-    page_output = @browser.link(:id => "bcKwText").text.include? "#{string}"
+    #@browser.link(:id => "bcKwText").wait_until_present
+    
+    #page_output = @browser.link(:id => "bcKwText").text.include? "#{string}"
+    #expect(page_output).to eql(true)
+
+    page_output = @browser.span(:class =>'a-text-bold').text.include? "#{string}"
     expect(page_output).to eql(true)
     @browser.close
 end
