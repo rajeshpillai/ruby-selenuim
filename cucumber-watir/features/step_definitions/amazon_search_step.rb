9eab1cf6dc6a805704b249abfa60d381cdf1071a
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-24 01:14:07 +0530

    Added cucumber feature: (todo -> as link id not found)

---
+++ cucumber-watir/features/step_definitions/amazon_search_step.rb
@@ -0,0 +1,16 @@
+Given("a user goes to Amazon home page") do
+    @browser.goto("http://www.amazon.com")
+end
+
+When("a user search for {string}") do |string|
+    @browser.text_field(:id => "twotabsearchtextbox").wait_until_present
+    @browser.text_field(:id => "twotabsearchtextbox").set "#{string}"
+    @browser.send_keys :return
+end
+
+Then("amazon should return result for {string}") do |string|
+    @browser.link(:id => "bcKwText").wait_until_present
+    page_output = @browser.link(:id => "bcKwText").text.include? "#{string}"
+    expect(page_output).to eql(true)
+    @browser.close
+end
